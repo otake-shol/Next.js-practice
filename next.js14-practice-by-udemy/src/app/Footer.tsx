a7b7b7106d9455b1a3e3071f3dad4236f9a137c4
otkshol <charm.ot.trog@gmail.com>
2024-06-09 11:35:30 +0900

    Footerの設定

---
+++ next.js14-practice-by-udemy/src/app/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="py-2 px-4 border-t">
+      <small>@2024 practice Footer</small>
+    </footer>
+  );
+};
--- next.js14-practice-by-udemy/src/app/layout.tsx
+++ next.js14-practice-by-udemy/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./Header";
+import { Footer } from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,7 @@ export default function RootLayout({
       <body className="container mx-auto text-slate-50">
         <Header />
         {children}
-        <footer>Test footer</footer>
+        <Footer />
       </body>
     </html>
   );
