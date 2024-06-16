9560d5fac7600a0596089f675a64a5bf7adc8680
otkshol <charm.ot.trog@gmail.com>
2024-06-16 23:20:03 +0900

    SSRを利用して情報を取得した内容を表示

--- next.js14-practice-by-udemy/src/app/components/ArticleList.tsx
+++ next.js14-practice-by-udemy/src/app/components/ArticleList.tsx
@@ -1,66 +1,47 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Article } from "@/types";
 
-const ArticleList = () => {
+type ArticleListProps = {
+  articles: Article[];
+};
+
+const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
-      <article className="shadow my-4 flex-col">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
-            alt=""
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <a href="#" className="text-blue-700 pb-4 font-bold">
-            Technology
-          </a>
-          <a
-            href="#"
-            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
-          >
-            Next.jsの学習
-          </a>
-          <p className="text-sm pb-3 text-slate-900">本文の概要はここに</p>
-          <a href="#" className=" text-slate-900 pb-6">
-            本文の記載はここに
-          </a>
-          <Link href="#" className="text-pink-800 hover:text-black">
-            続きを読む
+      {articles.map((article) => (
+        <article className="shadow my-4 flex-col" key={article.id}>
+          <Link href={`articles/${article.id}`} className="hover:opacity-75">
+            <Image
+              src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+              alt=""
+              width={1280}
+              height={300}
+            />
           </Link>
-        </div>
-      </article>
-      <article className="shadow my-4 flex-col">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://source.unsplash.com/collection/1346951/1000x500?sig=2"
-            alt=""
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <a href="#" className="text-blue-700 pb-4 font-bold">
-            Technology
-          </a>
-          <a
-            href="#"
-            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
-          >
-            Next.jsの学習
-          </a>
-          <p className="text-sm pb-3 text-slate-900">本文の概要はここに</p>
-          <a href="#" className=" text-slate-900 pb-6">
-            本文の記載はここに
-          </a>
-          <Link href="#" className="text-pink-800 hover:text-black">
-            続きを読む
-          </Link>
-        </div>
-      </article>
+          <div className="bg-white flex flex-col justify-start p-6">
+            <a href="#" className="text-blue-700 pb-4 font-bold">
+              Technology
+            </a>
+            <a
+              href="#"
+              className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+            >
+              {article.title}
+            </a>
+            <p className="text-sm pb-3 text-slate-900">
+              published on {article.createdAt}
+            </p>
+            <a href="#" className=" text-slate-900 pb-6">
+              {article.content}
+            </a>
+            <Link href="#" className="text-pink-800 hover:text-black">
+              続きを読む
+            </Link>
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
