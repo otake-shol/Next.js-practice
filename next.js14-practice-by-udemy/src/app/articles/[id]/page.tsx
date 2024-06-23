bd05deca1fb4ded2a3d381ff1ea7c359e37708e9
otkshol <charm.ot.trog@gmail.com>
2024-06-24 06:05:25 +0900

    詳細記事表示の修正

--- next.js14-practice-by-udemy/src/app/articles/[id]/page.tsx
+++ next.js14-practice-by-udemy/src/app/articles/[id]/page.tsx
@@ -9,14 +9,16 @@ const Article = async ({ params }: { params: { id: string } }) => {
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
-        src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`}
         alt=""
         width={1280}
         height={300}
       />
-      <h1 className="text-4xl text-center mb-10 mt-10">ブログタイトル</h1>
+      <h1 className="text-4xl text-center mb-10 mt-10">
+        {detailArticle.title}
+      </h1>
       <div className="text-lg leading-relaxed text-justify">
-        <p>本文</p>
+        <p>{detailArticle.content}</p>
       </div>
     </div>
   );
--- next.js14-practice-by-udemy/src/app/components/ArticleList.tsx
+++ next.js14-practice-by-udemy/src/app/components/ArticleList.tsx
@@ -14,18 +14,15 @@ const ArticleList = ({ articles }: ArticleListProps) => {
         <article className="shadow my-4 flex-col" key={article.id}>
           <Link href={`articles/${article.id}`} className="hover:opacity-75">
             <Image
-              src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
               alt=""
               width={1280}
               height={300}
             />
           </Link>
           <div className="bg-white flex flex-col justify-start p-6">
-            <a href="#" className="text-blue-700 pb-4 font-bold">
-              Technology
-            </a>
             <a
-              href="#"
+              href={`articles/${article.id}`}
               className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
             >
               {article.title}
